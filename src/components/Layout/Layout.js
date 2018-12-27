c01bf2b30145259b7b507a606d31ff3f2b4fe2c7
Igor Rocha <igortice@gmail.com>
2018-12-27 11:19:34 -0300

    init side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,11 +4,12 @@ import Aux from '../../hoc/Aux';
 
 import './layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => (
   <Aux>
     <Toolbar />
-
+    <SideDrawer />
     <main className={'Container'}>{props.children}</main>
   </Aux>
 );
